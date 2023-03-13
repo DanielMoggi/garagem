cf4f1596fe0259cc294d3731c553fed5fb6864c3
danielmoggi <danielmoggi2019@gmail.com>
2023-03-13 16:20:09 -0300

    crianção do modelo marcas

--- garagem/models.py
+++ garagem/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=100)
+    nacionalidade = models.CharField(nax_length=50)
+    
+
+    def __str__(self):
+        return self.nome.upper()
+
