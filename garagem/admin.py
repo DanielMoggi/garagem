b3380f73e13bfe3f8b1e825ab0d7e4ae28c99f66
danielmoggi <danielmoggi2019@gmail.com>
2023-03-13 16:27:03 -0300

    criação de alguma coisa

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Marca
 
-admin.site.regiter(Marca)
+admin.site.register(Marca)
 
 # Register your models here.
--- garagem/models.py
+++ garagem/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=100)
-    nacionalidade = models.CharField(nax_length=50)
-    
+    nacionalidade = models.CharField(max_length=50)
+
 
     def __str__(self):
         return self.nome.upper()
