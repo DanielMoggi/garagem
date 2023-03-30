f3d23868f41f85ee9e8370ef47e33400ea561d62
Daniel <danielmoggi2019@gmail.com>
2023-03-30 20:54:29 -0300

    comit 6.8

--- garagem/models.py
+++ garagem/models.py
@@ -4,18 +4,60 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50)
-
+    nacionalidade = models.CharField(max_length=50, null=True, blank=True)
+    
 
     def __str__(self):
         return self.nome.upper()
-    
-class Categoria(models.Model):
-    descrição = models.CharField(max_length=100)
- 
 
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descrição
+        return self.descricao
     
 
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.descricao
+       
+    class Meta:
+            verbose_name_plural = "Cores"
+    
+
+
+class Veiculo(models.Model):
+    Modelo = models.CharField(max_length=50)
+    Marca = models.ForeignKey(
+        Marca, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    Categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    Cor = models.ForeignKey(
+        Cor, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    Ano = models.IntegerField(
+        default=0, null=True, blank=True
+    )
+    Preco = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, default=0
+    )
+
+    def __str__(self):
+        return f"  {self.Modelo} {self.Marca} {self.Ano} {self.Cor}"
+        
+        
