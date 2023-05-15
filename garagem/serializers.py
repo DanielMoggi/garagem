59d186ebf65a949844811186dc31e7729d4736f1
Daniel <danielmoggi2019@gmail.com>
2023-05-15 14:25:14 -0300

    Uepa

--- garagem/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo, Modelo
-
-class AcessorioSerializer(ModelSerializer):
-    class Meta:
-        model = Acessorio
-        fields = "__all__"
-
--- garagem/views.py
+++ garagem/views.py
@@ -1,11 +1,3 @@
-from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import render
 
-from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo, Modelo
-from garagem.serializers import (
-    AcessorioSerializer
-    )
-
-class AcessorioViewSet(ModelViewSet):
-    queryset = Acessorio.objects.all()
-    serializer_class = AcessorioSerializer
-
+# Create your views here.
