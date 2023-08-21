c940fdd29a2c1f5aa487bbc67121112e14a7320b
DanielMoggi <danielmoggi2019@gmail.com>
2023-08-21 16:24:38 -0300

    URLS

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
@@ -35,11 +19,11 @@ from drf_spectacular.views import (
 
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
-router.register(r"acessorio", AcessorioViewSet)
-router.register(r"modelo", ModeloViewSet)
-router.register(r"marca", MarcaViewSet)
-router.register(r"veiculo", VeiculoViewSet)
-router.register(r"cor", CorViewSet)
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"modelos", ModeloViewSet)
+router.register(r"marcas", MarcaViewSet)
+router.register(r"veiculos", VeiculoViewSet)
+router.register(r"cores", CorViewSet)
 router.register(r"images", ImageUploadViewSet)
 
 urlpatterns = [
